38ecd15762e8da5dbb520da2f5579f9fff7ae8cf
Bruno Lima <bg.lima@hotmail.com>
2016-08-27 14:43:45 -0300

    Using FPS configs

--- main.lua
+++ main.lua
@@ -1,23 +1,31 @@
 function love.load()
+	-- Player configs
 	player = {posX=100, posY=100, vel=5}
+	
+	-- Timer configs
+	fps    = 60		-- Using 60 frames per second
+	min_dt = 1/fps
+	next_time = love.timer.getTime()
 end
 
 function love.draw()
-	love.graphics.rectangle( "fill", player.posX, player.posY, 16, 16)
+	-- Timer management
+	local cur_time = love.timer.getTime()
+	if next_time <= cur_time then
+		next_time = cur_time
+		return
+	end
+	love.timer.sleep(next_time - cur_time)	
 	
+	-- Drawing stuff
+	love.graphics.rectangle( "fill", player.posX, player.posY, 16, 16)	
 end
 
---[[
-function love.keydown(key)
-	if 		key == 'up' then 	player.posY = player.posY - player.vel
-	elseif	key == 'down' then	player.posY = player.posY + player.vel
-	elseif  key == 'right' then player.posX = player.posX + player.vel
-	elseif	key == 'left' then  player.posX = player.posX - player.vel
-	end
-		
-end ]]
-
 function love.update(dt) 
+	-- Updating time
+	next_time = next_time + min_dt
+	
+	-- Updating all the rest
 	readKeyboard()
 end
 
